fb0421fae2a616a280ea34ef1d59ecb5dc7528c9
徐翔 <xuxiang001@zhongan.com>
2023-04-12 10:49:36 +0800

    d

--- express/server.js
+++ express/server.js
@@ -11,7 +11,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 // 设置key
 dotenv.config()
-const GPT_API_KEY = "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" // process.env.GPT_API_KEY
+const GPT_API_KEY = "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" // process.env.GPT_API_KEY
 if (!GPT_API_KEY) {
     console.log("请配置 ChatGPT API Key")
 }
